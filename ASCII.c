e5746f9f014890a01cb14d2ec120a182048e7186
Lenin Mutum <119494001+hellolenin7@users.noreply.github.com>
2022-11-30 23:10:44 +0530

    Add files via upload

---
+++ ASCII.c
@@ -0,0 +1,9 @@
+// C programme for printing ASCII 
+#include <stdio.h>
+int main() {
+    char a;
+    printf("Enter the character:");
+    scanf("%c",&a);
+    printf("The ASCII value %c  is %d",a);
+    return 0;
+}
