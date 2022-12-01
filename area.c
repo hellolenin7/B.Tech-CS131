376d5a913de53a72d472c631dbb35ce896feb115
Lenin Mutum <119494001+hellolenin7@users.noreply.github.com>
2022-12-01 22:08:16 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c;
+    float s,area;
+    printf("Enter the values of the side of a triangle:\n");
+    scanf("%d%d%d",&a,&b,&c);
+    s=(a+b+c)/2;
+    area=sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("The area of the triangle is %.3f square unit\n",area);
+    return 0;
+};
+
