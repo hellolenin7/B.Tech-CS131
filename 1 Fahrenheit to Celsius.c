bc49b8b95f10803fd12ff4d0b9118fb66721c41e
Lenin Mutum <119494001+hellolenin7@users.noreply.github.com>
2022-12-01 21:34:54 +0530

    Update 1 Fahrenheit to Celsius.c

--- 1 Fahrenheit to Celsius.c
+++ 1 Fahrenheit to Celsius.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+int main()
+{
+    float f,c;
+    printf("Enter the temperature in Fahrenheit:\n");
+    scanf("%f",&f);
+    c=5.0/9*(f-32);
+    printf("The tempeerature in Celsius is %.3f",c);
+    return 0;
+}
