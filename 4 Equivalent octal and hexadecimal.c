0373059cba56822b0df0120eec6ab9467644ab0b
Lenin Mutum <119494001+hellolenin7@users.noreply.github.com>
2022-12-09 16:24:03 +0530

    Create 4 Equivalent octal and hexadecimal.c

---
+++ 4 Equivalent octal and hexadecimal.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter the decimal number:\n");
+    scanf("%d",&x);
+    printf("The equivalent octal and hexadecimal value of %d is %o and %x ", x,x,x);
+    return 0;
+}
