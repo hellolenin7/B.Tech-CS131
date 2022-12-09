fa508eab452e961baabbda17dad49d5d600a3ec1
Lenin Mutum <119494001+hellolenin7@users.noreply.github.com>
2022-12-09 17:17:28 +0530

    Create 7 Checking leap year or not.c

---
+++ 7 Checking leap year or not.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+    int y;
+    printf("Enter the year:\n");
+    scanf("%d",&y);
+    if(y%4==0 && y%100!=0)
+      printf("%d is leap year\n",y);
+    else if(y%4==0 && y%100==0 && y%400==0)
+      printf("%d is leap year\n",y);
+    else
+      printf("%d is not leap year\n",y);
+    return 0;
+}
